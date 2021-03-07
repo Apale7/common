ecfef8e46be15b29b3e18be8f6470c232cfd35e0
Apale7 <1092377056@qq.com>
2021-03-07 21:22:11 +0800

    add error code

--- constdef/error_code.go
+++ constdef/error_code.go
@@ -5,10 +5,12 @@ import "errors"
 const (
 	ErrInvalidParamsMsg = "invalid params"
 	ErrInvalidTokenMsg  = "invalid token"
+	ErrUnpackFailedMsg  = "unpack failed"
 )
 
 var (
 	// ErrInvalidParams error for invalid params
 	ErrInvalidParams = errors.New(ErrInvalidParamsMsg)
 	ErrInvalidToken  = errors.New(ErrInvalidTokenMsg)
+	ErrUnpackFailed  = errors.New(ErrUnpackFailedMsg)
 )
